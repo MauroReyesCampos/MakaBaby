86b5de1d1bf073c1fb584bf8805cc180bdf7b657
Mauricio <mreyesc@gmail.com>
2023-06-29 09:16:21 -0500

    new user page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NewuserComponent } from './pages/newuser/newuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'}, {path:'home', component: LoginComponent},
+  {path:'newuser', component: NewuserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
