24d11fe88b43af2d78830e4332322c07f32ed1cc
Mauricio <mreyesc@gmail.com>
2023-07-05 14:59:57 -0500

    header created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageService } from 'src/app/services/page.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(public pageService: PageService) { }
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageService } from 'src/app/services/page.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  constructor(private pageService: PageService) { }
+
+  ngOnInit() {
+    this.pageService.showHeader = false;
+  }
 
 }
